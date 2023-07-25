222bcaa662dedd85e48baa44fb28ad5de8aa29fb
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-07-24 23:37:35 -0400

    fixed database code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,12 @@ const initdb = async () =>
   });
 
 // Updates content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   // console.log("PUT to the database");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("data saved to the database", result);
 };
@@ -30,6 +30,6 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log("result.value", result);
-  return result.value;
+  return result?.value;
 };
 initdb();
